7c7ae27615921358ec731014198212ecae3cc293
jhagendoornCP <jhagendo@calpoly.edu>
2023-11-04 12:09:29 -0700

    starting to migrate to django

--- cpquantweb/base/urls.py
+++ cpquantweb/base/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path('', views.base, name='base'),
     path('page1/', views.page1, name='page1'),
     path('page2/', views.page2, name='page2'),
-    
+    path('get_data/', views.get_data, name='get_data'),
+    path('trades/', views.trades, name='trades'),
+    path('bars/', views.bars, name='bars'),
+    path('downloadbars/', views.bars, name='downloadbars/'),
 ]
